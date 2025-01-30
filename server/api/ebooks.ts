44df872f474dd52d9582f8d7a162c4f86910d65b
Rossignol Alex <alexnw33910@gmail.com>
2025-01-30 15:02:18 +0100

    update server

--- server/api/ebooks.ts
+++ server/api/ebooks.ts
@@ -7,12 +7,38 @@ export default defineEventHandler(async (event: H3Event) => {
   try {
     const { type, query, limit } = getQuery(event);
 
-    const maxResults = limit || "10";
+    const randomSubject = () => {
+      const subject = [
+        "fiction",
+        "mystery",
+        "science",
+        "history",
+        "romance",
+        "adventure",
+        "biography",
+        "technology",
+        "art",
+        "psychology",
+        "travel",
+        "business",
+        "self-help",
+        "health",
+        "religion",
+        "literature",
+        "horror",
+        "philosophy",
+      ];
+
+      const randomIndex = Math.floor(Math.random() * subject.length);
+      return subject[randomIndex];
+    };
+
+    const maxResults = limit || "20";
 
     let request;
 
     if (!type || !query) {
-      request = `${googleApiURL}?q=random&maxResults=${maxResults}`;
+      request = `${googleApiURL}?q=subject:${randomSubject()}&subject:${randomSubject()}&subject:${randomSubject()}&maxResults=${maxResults}`;
     } else {
       request = `${googleApiURL}?q=${type}:${query}&maxResults=${maxResults}`;
     }
