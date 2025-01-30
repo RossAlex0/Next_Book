e22470024dc797cfb8efcbbe0064dfd013be5e7c
Rossignol Alex <alexnw33910@gmail.com>
2025-01-30 15:04:41 +0100

    add component folder

---
+++ components/Card/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { ApiData } from "~/utils/services/type";
+import "./index.css";
+
+defineProps<{ ebook: ApiData }>();
+</script>
+
+<template>
+  <div class="card_element">
+    <div>
+      <img
+        v-bind:src="ebook.volumeInfo.imageLinks.smallThumbnail"
+        v-bind:alt="ebook.volumeInfo.title"
+        v-if="ebook.volumeInfo.imageLinks?.smallThumbnail"
+      />
+      <p :style="{ fontSize: '22px', fontWeight: 'bold' }" v-else>?</p>
+    </div>
+    <div>
+      <p v-if="ebook.volumeInfo.authors?.length">
+        {{ ebook.volumeInfo.authors[0] }}
+      </p>
+      <p v-else>...</p>
+    </div>
+    <div>
+      <p v-if="ebook.volumeInfo.title">{{ ebook.volumeInfo.title }}</p>
+      <p v-else>...</p>
+    </div>
+    <div>
+      <p v-if="ebook.volumeInfo.categories">
+        {{ ebook.volumeInfo.categories[0] }}
+      </p>
+      <p v-else>...</p>
+    </div>
+    <div>
+      <a
+        v-bind:href="ebook.saleInfo.buyLink"
+        target="_blank"
+        :style="!ebook.saleInfo.buyLink && { opacity: 0.5 }"
+        >Acheter</a
+      >
+    </div>
+  </div>
+</template>
---
+++ components/NavAside/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import "./index.css";
+</script>
+
+<template>
+  <aside class="nav_container">
+    <div class="aside_contain">
+      <h1>- Ebook -</h1>
+      <p>
+        Découvrez Next eBook, la plateforme ultime pour explorer et trouver des
+        e-books adaptés à vos envies. Plongez dans un univers de lecture sans
+        limite et trouvez votre prochain coup de cœur littéraire dès maintenant
+        !
+      </p>
+    </div>
+    <div class="aside_input_container">
+      <div><label>Titre:</label><SearchInput type="intitle" /></div>
+      <div><label>Genre:</label><SearchInput type="subject" /></div>
+      <div><label>Auteur:</label><SearchInput type="inauthor" /></div>
+    </div>
+    <div class="aside_contain"><p>Copy-2024-Ross0Dev</p></div>
+  </aside>
+</template>
